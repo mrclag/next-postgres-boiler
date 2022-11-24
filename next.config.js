6a0147a2954faad7980b98a75a7acfb401bc18a5
Matt Clagett <mattrclagett@gmail.com>
2022-11-24 17:09:42 -0600

    setup for deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if your project has type errors.
+    ignoreBuildErrors: true,
+  },
+}
